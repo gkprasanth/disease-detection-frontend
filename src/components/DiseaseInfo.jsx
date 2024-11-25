5ec9ff5f7fd40246b7bb7048356731b752c25aa3
gkprasanth <gkpc2004@gmail.com>
2024-11-25 13:30:01 +0530

    UI

--- src/components/DiseaseInfo.jsx
+++ src/components/DiseaseInfo.jsx
@@ -1,11 +1,12 @@
 function DiseaseInfo({ disease, info }) {
-    return (
-      <div className="bg-gray-100 p-4 rounded shadow">
-        <h2 className="text-lg font-bold">{disease}</h2>
+  return (
+    <div className="mt-6 p-6 bg-white rounded-lg shadow-lg max-w-lg mx-auto">
+      <h2 className="text-2xl font-semibold text-center text-gray-800 mb-4">{disease}</h2>
+      <div className="text-lg text-gray-800">
         <p>{info}</p>
       </div>
-    );
-  }
-  
-  export default DiseaseInfo;
-  
+    </div>
+  );
+}
+
+export default DiseaseInfo;
